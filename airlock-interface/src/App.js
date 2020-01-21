87760d37642aebb9937d06ad7dae39fcaedcc483
Alex Palmer <alex@rivi.io>
2020-01-21 16:10:00 +1100

    IP now modifiable variable

--- airlock-interface/src/App.js
+++ airlock-interface/src/App.js
@@ -2,34 +2,33 @@ import "./App.css";
 
 import { Route, HashRouter as Router, Switch } from "react-router-dom";
 
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "@apollo/react-hooks";
 import Clinician from "./pages/Clinician";
 import Header from "./components/Header";
 import Landing from "./pages/Landing";
 import React from "react";
 import Researcher from "./pages/Researcher";
-import logo from "./logo.svg";
-
-const client = new ApolloClient({
-  uri: "http://localhost:8000"
-});
+import Validation from "./pages/Validation";
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div>
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/clinician" component={Clinician} />
-            <Route exact path="/researcher" component={Researcher} />
-          </Switch>
-        </Router>
-      </div>
-    </ApolloProvider>
-  );
+    const IP = "172.21.78.131";
+
+    return (
+        <div>
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/clinician" component={Clinician} />
+                    <Route
+                        exact
+                        path="/researcher"
+                        render={() => <Researcher IP={IP} />}
+                    />
+                    <Route exact path="/validation" component={Validation} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
