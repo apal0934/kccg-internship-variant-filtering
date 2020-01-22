0e5a52e30b027735914754ea258a8562c254e7a7
Alex Palmer <alex@rivi.io>
2020-01-22 14:46:46 +1100

    just python things

--- dummy_genome_database/main.py
+++ dummy_genome_database/main.py
@@ -1,5 +1,6 @@
 import graphene
 from fastapi import FastAPI
+from mongoengine import connect, disconnect_all
 from starlette.graphql import GraphQLApp
 
 
@@ -11,4 +12,16 @@ class Query(graphene.ObjectType):
 
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def connect_db_client():
+    connect("genomes")
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    disconnect_all
+
+
 app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
