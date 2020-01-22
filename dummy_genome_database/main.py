a5edd38151edb9c3472983f809075655df6f942f
Alex Palmer <alex@rivi.io>
2020-01-22 17:18:07 +1100

    filter results by consenting and variant match

--- dummy_genome_database/main.py
+++ dummy_genome_database/main.py
@@ -9,10 +9,12 @@ from dummy_genome_database.object_types.genome import Genome
 
 
 class Query(graphene.ObjectType):
-    genomes = graphene.List(Genome)
+    genomes = graphene.List(Genome, genome_ids=graphene.List(graphene.Int), variant_id=graphene.Int())
     genome = graphene.Field(Genome, genome_id=graphene.String())
 
-    def resolve_genomes(self, info):
+    def resolve_genomes(self, info, genome_ids=None, variant_id=None):
+        if genome_ids and variant_id:
+            return list(GenomeModel.objects(genome_id__in=genome_ids, variants=variant_id))
         return list(GenomeModel.objects.all())
 
     def resolve_genome(self, info, genome_id):
