2a195b016d9474231ea650e7cc11ae2f0a4bb942
Alex Palmer <alex@rivi.io>
2020-01-24 14:02:24 +1100

    Added query of variants

--- dummy_genome_database/main.py
+++ dummy_genome_database/main.py
@@ -7,11 +7,15 @@ from starlette.middleware.cors import CORSMiddleware
 from dummy_genome_database.models.genome import GenomeModel
 from dummy_genome_database.mutations.mutations import Mutations
 from dummy_genome_database.object_types.genome import Genome
+from dummy_genome_database.object_types.variant import Variant
+from dummy_genome_database.models.variant import VariantModel
 
 
 class Query(graphene.ObjectType):
     genomes = graphene.List(Genome, genome_ids=graphene.List(graphene.Int), variant_id=graphene.Int())
     genome = graphene.Field(Genome, genome_id=graphene.String())
+
+    variants = graphene.List(Variant)
 
     def resolve_genomes(self, info, genome_ids=None, variant_id=None):
         if genome_ids and variant_id:
@@ -20,6 +24,9 @@ class Query(graphene.ObjectType):
 
     def resolve_genome(self, info, genome_id):
         return GenomeModel.objects.get(pk=genome_id)
+
+    def resolve_variants(self, info):
+        return list(VariantModel.objects.all())
 
 
 app = FastAPI()
