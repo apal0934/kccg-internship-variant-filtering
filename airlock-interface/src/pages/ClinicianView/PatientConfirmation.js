e7479f6fbaff948868679b5b0434db0cbb7e2022
Alex Palmer <alex@rivi.io>
2020-02-26 16:16:24 +1100

    Add missing fade to patient confirmation

--- airlock-interface/src/pages/ClinicianView/PatientConfirmation.js
+++ airlock-interface/src/pages/ClinicianView/PatientConfirmation.js
@@ -1,6 +1,8 @@
 import { Button, Checkbox, Descriptions, Divider, Form } from "antd";
 import React, { Component } from "react";
 
+import { Fade } from "react-reveal";
+
 class PatientConfirmation extends Component {
   onSubmit = e => {
     e.preventDefault();
@@ -16,45 +18,47 @@ class PatientConfirmation extends Component {
 
     return (
       <div>
-        <Descriptions
-          title="Patient Info"
-          layout="vertical"
-          size="small"
-          bordered
-        >
-          <Descriptions.Item label="First name">
-            {this.props.userData.firstName}
-          </Descriptions.Item>
-          <Descriptions.Item label="Last name">
-            {this.props.userData.lastName}
-          </Descriptions.Item>
-          <Descriptions.Item label="Date of birth">
-            {this.props.userData.dateOfBirth.split("T")[0]}
-          </Descriptions.Item>
-          <Descriptions.Item label="Patient ID">
-            {this.props.userData.userId}
-          </Descriptions.Item>
-          <Descriptions.Item label="Sample ID">
-            {this.props.mappingData}
-          </Descriptions.Item>
-        </Descriptions>
-        <Divider>Please review these details</Divider>
-        <Form onSubmit={this.onSubmit}>
-          <Form.Item>
-            {getFieldDecorator("confirm")(
-              <Checkbox>I confirm these details to be correct</Checkbox>
-            )}
-          </Form.Item>
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={!getFieldValue("confirm")}
-            >
-              Next
-            </Button>
-          </Form.Item>
-        </Form>
+        <Fade>
+          <Descriptions
+            title="Patient Info"
+            layout="vertical"
+            size="small"
+            bordered
+          >
+            <Descriptions.Item label="First name">
+              {this.props.userData.firstName}
+            </Descriptions.Item>
+            <Descriptions.Item label="Last name">
+              {this.props.userData.lastName}
+            </Descriptions.Item>
+            <Descriptions.Item label="Date of birth">
+              {this.props.userData.dateOfBirth.split("T")[0]}
+            </Descriptions.Item>
+            <Descriptions.Item label="Patient ID">
+              {this.props.userData.userId}
+            </Descriptions.Item>
+            <Descriptions.Item label="Sample ID">
+              {this.props.mappingData}
+            </Descriptions.Item>
+          </Descriptions>
+          <Divider>Please review these details</Divider>
+          <Form onSubmit={this.onSubmit}>
+            <Form.Item>
+              {getFieldDecorator("confirm")(
+                <Checkbox>I confirm these details to be correct</Checkbox>
+              )}
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                disabled={!getFieldValue("confirm")}
+              >
+                Next
+              </Button>
+            </Form.Item>
+          </Form>
+        </Fade>
       </div>
     );
   }
